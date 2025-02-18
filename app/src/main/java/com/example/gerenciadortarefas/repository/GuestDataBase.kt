ab1109b78804e0942a5c94aedca711a8a9a0f1e6
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-17 22:00:25 -0300

    .

--- app/src/main/java/com/example/gerenciadortarefas/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/gerenciadortarefas/repository/GuestDataBase.kt
@@ -20,7 +20,7 @@ class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VE
                     DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, " +
                     DataBaseConstants.GUEST.COLUMNS.NAME + " text, " +
                     DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer, " +
-                    DataBaseConstants.GUEST.COLUMNS.DESCRICAO + "text);"
+                    DataBaseConstants.GUEST.COLUMNS.DESCRICAO + " text);"
         )
     }
 
